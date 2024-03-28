119e9ee5fbff46bbc2937a7183cb28532a719236
Oleksii Kozlov <oleksiikozlov@gmail.com>
2024-03-28 09:21:13 +0200

    make build

---
+++ index.7f356328.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",slidesPerView:1,spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{768:{slidesPerView:2}}});
+//# sourceMappingURL=index.7f356328.js.map
---
+++ index.aa69868b.js
@@ -0,0 +1,17 @@
+/* eslint-disable */ "use strict";
+const e = new Swiper(".swiper", {
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: !0
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2
+        }
+    }
+});
+
+//# sourceMappingURL=index.aa69868b.js.map
